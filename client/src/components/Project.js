a9380d3b8645ad586431d2f275769e4c8dd085b7
Trey McGarity <trey.mcgarity99@gmail.com>
2020-02-22 13:28:40 -0600

    created app and showing all projects, working on nav to details

---
+++ client/src/components/Project.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Project.css';
+
+const Project = props => {
+    const {name, description} = props.project;
+
+    return (
+        <div className="project-card">
+            <h3>{name}</h3>
+            <p>{description}</p>
+        </div>
+    )
+}
+export default Project;
